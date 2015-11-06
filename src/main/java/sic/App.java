141b7cb71c7c937e6752635307acd00a6d00d725
Franco Bellucci <belluccifranco@gmail.com>
2015-11-06 10:23:34 -0300

    Agrega dependencias de Spring para DI.

--- src/main/java/sic/App.java
+++ src/main/java/sic/App.java
@@ -1,14 +1,19 @@
 package sic;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import sic.vista.swing.GUI_LogIn;
 
 /**
  * Esta es la clase principal que arranca la aplicación.
  */
+@Configuration
+@ComponentScan
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         GUI_LogIn gui_LogIn = new GUI_LogIn();
         gui_LogIn.setVisible(true);
-    }
+    }   
 }
