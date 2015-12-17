37eba691fef4ac090e2030653dbc8ccfc96a224e
Franco Bellucci <belluccifranco@gmail.com>
2015-12-17 18:32:11 -0300

    Realiza cambios menores en el reporte de Pedido y cambia el nombre de la tabla para renglon pedido a minusculas.

--- src/main/java/sic/modelo/RenglonPedido.java
+++ src/main/java/sic/modelo/RenglonPedido.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "renglonPedido")
+@Table(name = "renglonpedido")
 @Data
 public class RenglonPedido implements Serializable {
 
