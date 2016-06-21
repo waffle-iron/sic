5664745e815baddae0dbc50f1d80effff9406827
Jose Francisco Mayol <mayol.jose.f@gmail.com>
2016-06-21 19:04:36 -0300

    Mayol Jose Francisco - Agrega testing para el service de Factura.

--- src/test/java/sic/service/FacturaServiceTest.java
+++ src/test/java/sic/service/FacturaServiceTest.java
@@ -1,159 +1,207 @@
 package sic.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import sic.modelo.Cliente;
 import sic.modelo.CondicionIVA;
 import sic.modelo.Empresa;
+import sic.modelo.FacturaVenta;
+import sic.modelo.Medida;
+import sic.modelo.Producto;
 import sic.modelo.Proveedor;
+import sic.modelo.RenglonFactura;
+import sic.repository.IFacturaRepository;
+import sic.service.impl.FacturaServiceImpl;
+import sic.service.impl.ProductoServiceImpl;
 
 public class FacturaServiceTest {
 
-    
-//    @Test
-//    public void testGetTipoFacturaCompraCuandoEmpresaYProveedorDiscriminan() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVAqueDiscrimina = new CondicionIVA();
-//        condicionIVAqueDiscrimina.setDiscriminaIVA(true);
-//        empresa.setCondicionIVA(condicionIVAqueDiscrimina);
-//        Proveedor proveedor = new Proveedor();
-//        proveedor.setCondicionIVA(condicionIVAqueDiscrimina);
-//        FacturaService instance = new FacturaService();
-//        char[] expResult = {'A', 'B', 'X'};
-//        char[] result = instance.getTipoFacturaCompra(empresa, proveedor);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaCompraCuandoEmpresaDiscriminaYProveedorNO() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVAqueDiscrimina = new CondicionIVA();
-//        condicionIVAqueDiscrimina.setDiscriminaIVA(true);
-//        empresa.setCondicionIVA(condicionIVAqueDiscrimina);
-//        Proveedor proveedor = new Proveedor();
-//        CondicionIVA condicionIVAqueNoDiscrimina = new CondicionIVA();
-//        condicionIVAqueNoDiscrimina.setDiscriminaIVA(false);
-//        proveedor.setCondicionIVA(condicionIVAqueNoDiscrimina);
-//        FacturaService instance = new FacturaService();
-//        char[] expResult = {'C', 'X'};
-//        empresa.getCondicionIVA().isDiscriminaIVA();
-//        char[] result = instance.getTipoFacturaCompra(empresa, proveedor);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaCompraCuandoEmpresaNoDiscriminaYProveedorSI() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVAqueNoDiscrimina = new CondicionIVA();
-//        condicionIVAqueNoDiscrimina.setDiscriminaIVA(false);
-//        empresa.setCondicionIVA(condicionIVAqueNoDiscrimina);
-//        Proveedor proveedor = new Proveedor();
-//        CondicionIVA condicionIVAqueDiscrimina = new CondicionIVA();
-//        condicionIVAqueDiscrimina.setDiscriminaIVA(true);
-//        proveedor.setCondicionIVA(condicionIVAqueDiscrimina);
-//        FacturaService instance = new FacturaService();
-//        char[] expResult = {'B', 'X'};
-//        empresa.getCondicionIVA().isDiscriminaIVA();
-//        char[] result = instance.getTipoFacturaCompra(empresa, proveedor);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaCompraCuandoEmpresaNoDiscriminaYProveedorTampoco() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVAqueNoDiscrimina = new CondicionIVA();
-//        condicionIVAqueNoDiscrimina.setDiscriminaIVA(false);
-//        empresa.setCondicionIVA(condicionIVAqueNoDiscrimina);
-//        Proveedor proveedor = new Proveedor();
-//        proveedor.setCondicionIVA(condicionIVAqueNoDiscrimina);
-//        FacturaService instance = new FacturaService();
-//        char[] expResult = {'C', 'X'};
-//        empresa.getCondicionIVA().isDiscriminaIVA();
-//        char[] result = instance.getTipoFacturaCompra(empresa, proveedor);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaVenta() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVA = new CondicionIVA();
-//        condicionIVA.setDiscriminaIVA(true);
-//        empresa.setCondicionIVA(condicionIVA);
-//        Cliente cliente = new Cliente();
-//        cliente.setCondicionIVA(condicionIVA);
-//        FacturaService instance = new FacturaService();
-//        String[] expResult = {"Factura A", "Factura B", "Factura X", "Factura Y", "Pedido"};
-//        String[] result = instance.getTipoFacturaVenta(empresa, cliente);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaVentaEmpresaDiscriminaYClienteNo() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVA = new CondicionIVA();
-//        condicionIVA.setDiscriminaIVA(true);
-//        empresa.setCondicionIVA(condicionIVA);
-//        Cliente cliente = new Cliente();
-//        CondicionIVA condicionIVAnoDiscrimina = new CondicionIVA();
-//        condicionIVAnoDiscrimina.setDiscriminaIVA(false);
-//        cliente.setCondicionIVA(condicionIVAnoDiscrimina);
-//        FacturaService instance = new FacturaService();
-//        String[] expResult = {"Factura B", "Factura X", "Factura Y", "Pedido"};
-//        String[] result = instance.getTipoFacturaVenta(empresa, cliente);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaVentaEmpresaNoDiscriminaYClienteSi() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVAnoDiscrimina = new CondicionIVA();
-//        condicionIVAnoDiscrimina.setDiscriminaIVA(false);
-//        empresa.setCondicionIVA(condicionIVAnoDiscrimina);
-//        Cliente cliente = new Cliente();
-//        CondicionIVA condicionIVA = new CondicionIVA();
-//        condicionIVA.setDiscriminaIVA(true);
-//        cliente.setCondicionIVA(condicionIVA);
-//        FacturaService instance = new FacturaService();
-//        String[] expResult = {"Factura C", "Factura X", "Factura Y", "Pedido"};
-//        String[] result = instance.getTipoFacturaVenta(empresa, cliente);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTipoFacturaVentaEmpresaNoDiscriminaYClienteNO() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVAnoDiscrimina = new CondicionIVA();
-//        condicionIVAnoDiscrimina.setDiscriminaIVA(false);
-//        empresa.setCondicionIVA(condicionIVAnoDiscrimina);
-//        Cliente cliente = new Cliente();
-//        cliente.setCondicionIVA(condicionIVAnoDiscrimina);
-//        FacturaService instance = new FacturaService();
-//        String[] expResult = {"Factura C", "Factura X", "Factura Y", "Pedido"};
-//        String[] result = instance.getTipoFacturaVenta(empresa, cliente);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTiposFacturaSegunEmpresaSiDiscriminaIVA() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVA = new CondicionIVA();
-//        condicionIVA.setDiscriminaIVA(true);
-//        empresa.setCondicionIVA(condicionIVA);
-//        FacturaService instance = new FacturaService();
-//        char[] expResult = {'A', 'B', 'X', 'Y'};
-//        char[] result = instance.getTiposFacturaSegunEmpresa(empresa);
-//        assertArrayEquals(expResult, result);
-//    }
-//
-//    @Test
-//    public void testGetTiposFacturaSegunEmpresaSiNoDiscriminaIVA() {
-//        Empresa empresa = new Empresa();
-//        CondicionIVA condicionIVA = new CondicionIVA();
-//        condicionIVA.setDiscriminaIVA(false);
-//        empresa.setCondicionIVA(condicionIVA);
-//        FacturaService instance = new FacturaService();
-//        char[] expResult = {'C', 'X', 'Y'};
-//        char[] result = instance.getTiposFacturaSegunEmpresa(empresa);
-//        assertArrayEquals(expResult, result);
-//    }
+    private IFacturaService facturaService;
+    private IProductoService productoService;
+    private Producto producto;
+
+    @Before
+    public void before() {
+        productoService = Mockito.mock(ProductoServiceImpl.class);
+        IFacturaRepository facturaRepository = Mockito.mock(IFacturaRepository.class);
+        when(facturaRepository.getMayorNumFacturaSegunTipo("", (long) 1)).thenReturn((long) 1);
+
+        facturaService = new FacturaServiceImpl(facturaRepository, productoService, null, null, null);
+
+    }
+
+    @Test
+    public void testGetTipoFacturaCompraCuandoEmpresaYProveedorDiscriminan() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        empresa.setCondicionIVA(condicionIVAqueDiscrimina);
+        Proveedor proveedor = Mockito.mock(Proveedor.class);
+        when(proveedor.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        char[] expResult = {'A', 'B', 'X'};
+        char[] result = facturaService.getTipoFacturaCompra(empresa, proveedor);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaCompraCuandoEmpresaDiscriminaYProveedorNO() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Proveedor proveedor = Mockito.mock(Proveedor.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        when(proveedor.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        char[] expResult = {'C', 'X'};
+        char[] result = facturaService.getTipoFacturaCompra(empresa, proveedor);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaCompraCuandoEmpresaNoDiscriminaYProveedorSI() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Proveedor proveedor = Mockito.mock(Proveedor.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        when(proveedor.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        char[] expResult = {'B', 'X'};
+        empresa.getCondicionIVA().isDiscriminaIVA();
+        char[] result = facturaService.getTipoFacturaCompra(empresa, proveedor);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaCompraCuandoEmpresaNoDiscriminaYProveedorTampoco() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Proveedor proveedor = Mockito.mock(Proveedor.class);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        when(proveedor.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        char[] expResult = {'C', 'X'};
+        empresa.getCondicionIVA().isDiscriminaIVA();
+        char[] result = facturaService.getTipoFacturaCompra(empresa, proveedor);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaVentaCuandoEmpresaDiscriminaYClienteTambien() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Cliente cliente = Mockito.mock(Cliente.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        when(cliente.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        String[] expResult = {"Factura A", "Factura B", "Factura X", "Factura Y", "Pedido"};
+        String[] result = facturaService.getTipoFacturaVenta(empresa, cliente);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaVentaEmpresaDiscriminaYClienteNo() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Cliente cliente = Mockito.mock(Cliente.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        when(cliente.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        String[] expResult = {"Factura B", "Factura X", "Factura Y", "Pedido"};
+        String[] result = facturaService.getTipoFacturaVenta(empresa, cliente);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaVentaEmpresaNoDiscriminaYClienteSi() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Cliente cliente = Mockito.mock(Cliente.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        when(cliente.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        String[] expResult = {"Factura C", "Factura X", "Factura Y", "Pedido"};
+        String[] result = facturaService.getTipoFacturaVenta(empresa, cliente);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTipoFacturaVentaEmpresaNoDiscriminaYClienteNO() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        Cliente cliente = Mockito.mock(Cliente.class);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        when(cliente.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        String[] expResult = {"Factura C", "Factura X", "Factura Y", "Pedido"};
+        String[] result = facturaService.getTipoFacturaVenta(empresa, cliente);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTiposFacturaSegunEmpresaSiDiscriminaIVA() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        CondicionIVA condicionIVAqueDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.TRUE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueDiscrimina);
+        char[] expResult = {'A', 'B', 'X', 'Y'};
+        char[] result = facturaService.getTiposFacturaSegunEmpresa(empresa);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetTiposFacturaSegunEmpresaSiNoDiscriminaIVA() {
+        Empresa empresa = Mockito.mock(Empresa.class);
+        CondicionIVA condicionIVAqueNoDiscrimina = Mockito.mock(CondicionIVA.class);
+        when(condicionIVAqueNoDiscrimina.isDiscriminaIVA()).thenReturn(Boolean.FALSE);
+        when(empresa.getCondicionIVA()).thenReturn(condicionIVAqueNoDiscrimina);
+        char[] expResult = {'C', 'X', 'Y'};
+        char[] result = facturaService.getTiposFacturaSegunEmpresa(empresa);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testDividirFactura() {
+        System.out.println("dividirFactura");
+        FacturaVenta factura = Mockito.mock(FacturaVenta.class);
+        RenglonFactura uno = Mockito.mock(RenglonFactura.class);
+        RenglonFactura dos = Mockito.mock(RenglonFactura.class);
+        producto = Mockito.mock(Producto.class);
+        when(producto.getId_Producto()).thenReturn((long) 1);
+        when(producto.getCodigo()).thenReturn("1");
+        when(producto.getDescripcion()).thenReturn("producto test");
+        Medida medida = Mockito.mock(Medida.class);
+        when(producto.getMedida()).thenReturn(medida);
+        when(producto.getPrecioVentaPublico()).thenReturn(1.0);
+        when(producto.getIva_porcentaje()).thenReturn(21.00);
+        when(producto.getImpuestoInterno_porcentaje()).thenReturn(0.0);
+        when(producto.getPrecioLista()).thenReturn(1.0);
+        when(productoService.getProductoPorId((long) 1)).thenReturn(producto);
+        when(uno.getId_ProductoItem()).thenReturn((long) 1);
+        when(dos.getId_ProductoItem()).thenReturn((long) 1);
+        when(uno.getCantidad()).thenReturn(4.00);
+        when(dos.getCantidad()).thenReturn(7.00);
+        List<RenglonFactura> renglones = new ArrayList<>();
+        renglones.add(uno);
+        renglones.add(dos);
+        when(factura.getRenglones()).thenReturn(renglones);
+        when(factura.getTipoFactura()).thenReturn('A');
+        int[] indices = {0, 1};
+        int cantidadDeFacturasEsperadas = 2;
+        List<FacturaVenta> result = facturaService.dividirFactura(factura, indices);
+        assertEquals(cantidadDeFacturasEsperadas, result.size());
+    }
 }
