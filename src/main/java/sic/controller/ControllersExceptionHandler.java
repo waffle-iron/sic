65822b64aa2e6c2eba1d2ab9b0a125058a664735
Franco Bellucci <belluccifranco@gmail.com>
2016-09-01 20:06:38 -0300

    Nuevo exception handler para los controllers

---
+++ src/main/java/sic/controller/ControllersExceptionHandler.java
@@ -0,0 +1,31 @@
+package sic.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import sic.service.ServiceException;
+
+@ControllerAdvice
+public class ControllersExceptionHandler {
+    
+    private static final Logger LOGGER = Logger.getLogger(ControllersExceptionHandler.class.getPackage().getName());
+
+    @ExceptionHandler(ServiceException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleServiceException(ServiceException ex) {
+        LOGGER.error(ex.getMessage());
+        return ex.getMessage();
+    }    
+    
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleException(Exception ex) {
+        LOGGER.error(ex.getMessage());
+        return ex.getMessage();
+    }
+}
