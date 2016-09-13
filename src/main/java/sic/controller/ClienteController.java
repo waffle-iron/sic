a45e772f5c30abebcd6511def93d8ad6b5a9455a
Franco Bellucci <belluccifranco@gmail.com>
2016-09-12 21:35:17 -0300

    Cambios menores en base al code review

--- src/main/java/sic/controller/ClienteController.java
+++ src/main/java/sic/controller/ClienteController.java
@@ -3,7 +3,6 @@ package sic.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +15,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import sic.modelo.BusquedaClienteCriteria;
 import sic.modelo.Cliente;
-import sic.modelo.Empresa;
-import sic.modelo.Localidad;
-import sic.modelo.Pais;
-import sic.modelo.Provincia;
 import sic.service.IClienteService;
 import sic.service.IEmpresaService;
 import sic.service.ILocalidadService;
@@ -37,9 +32,9 @@ public class ClienteController {
     private final ILocalidadService localidadService;
     
     @Autowired
-    public ClienteController(IClienteService clienteService, IEmpresaService empresaService,IPaisService paisService, 
-            IProvinciaService provinciaService, ILocalidadService localidadService) {
-        
+    public ClienteController(IClienteService clienteService, IEmpresaService empresaService,
+            IPaisService paisService, IProvinciaService provinciaService,
+            ILocalidadService localidadService) {
         this.clienteService = clienteService;
         this.empresaService = empresaService;
         this.paisService = paisService;
@@ -55,42 +50,40 @@ public class ClienteController {
     
     @GetMapping("/clientes/criteria") 
     @ResponseStatus(HttpStatus.OK)
-    public List<Cliente>  buscarConCriteria(@RequestParam(value = "razonSocial", required = false) String razonSocial,
-                                  @RequestParam(value = "nombreFantasia", required = false) String nombreFantasia,
-                                  @RequestParam(value = "idFiscal", required = false) String idFiscal,
-                                  @RequestParam(value = "idPais", required = false) Long idPais,
-                                  @RequestParam(value = "idProvincia", required = false) Long idProvincia, 
-                                  @RequestParam(value = "idLocalidad", required = false) Long idLocalidad, 
-                                  @RequestParam(value = "idEmpresa") Long idEmpresa) {
-        List<Cliente> clientes = clienteService.buscarClientes(
-                                  new BusquedaClienteCriteria(
-                                  (razonSocial != null), razonSocial, 
-                                  (nombreFantasia != null), nombreFantasia, 
-                                  (idFiscal != null), idFiscal, 
-                                  (idPais != null), paisService.getPaisPorId(idPais), 
-                                  (idProvincia != null), provinciaService.getProvinciaPorId(idProvincia), 
-                                  (idLocalidad != null), localidadService.getLocalidadPorId(idLocalidad),
-                                  empresaService.getEmpresaPorId(idEmpresa))
-                                  ); 
-        return clientes;
-    } 
+    public List<Cliente> buscarConCriteria(@RequestParam(value = "razonSocial", required = false) String razonSocial,
+                                           @RequestParam(value = "nombreFantasia", required = false) String nombreFantasia,
+                                           @RequestParam(value = "idFiscal", required = false) String idFiscal,
+                                           @RequestParam(value = "idPais", required = false) Long idPais,
+                                           @RequestParam(value = "idProvincia", required = false) Long idProvincia, 
+                                           @RequestParam(value = "idLocalidad", required = false) Long idLocalidad, 
+                                           @RequestParam(value = "idEmpresa") Long idEmpresa) {
+        return clienteService.buscarClientes(
+                new BusquedaClienteCriteria(
+                (razonSocial != null), razonSocial, 
+                (nombreFantasia != null), nombreFantasia, 
+                (idFiscal != null), idFiscal, 
+                (idPais != null), paisService.getPaisPorId(idPais), 
+                (idProvincia != null), provinciaService.getProvinciaPorId(idProvincia), 
+                (idLocalidad != null), localidadService.getLocalidadPorId(idLocalidad),
+                empresaService.getEmpresaPorId(idEmpresa)));
+    }
        
     @GetMapping("/clientes/empresa/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public List<Cliente> getClientes(@PathVariable("id") long Id){
-        return clienteService.getClientes(empresaService.getEmpresaPorId(Id));
+    public List<Cliente> getClientes(@PathVariable("id") long id) {
+        return clienteService.getClientes(empresaService.getEmpresaPorId(id));
     }
     
     @GetMapping("/clientes/predeterminado/empresa/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Cliente getClientePredeterminado(@PathVariable("id") long id){
+    public Cliente getClientePredeterminado(@PathVariable("id") long id) {
      return clienteService.getClientePredeterminado(empresaService.getEmpresaPorId(id));
     }
     
     
     @DeleteMapping("/clientes/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void eliminar(@PathVariable("Id") long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable("id") long id) {
         clienteService.eliminar(clienteService.getClientePorId(id));
     }
     
@@ -103,14 +96,14 @@ public class ClienteController {
     
     @PutMapping("/clientes")
     @ResponseStatus(HttpStatus.OK)
-    public Cliente actualizar(@RequestBody Cliente cliente){
+    public Cliente actualizar(@RequestBody Cliente cliente) {
        clienteService.actualizar(cliente);
        return clienteService.getClientePorId(cliente.getId_Cliente());
     }
     
     @PutMapping("/clientes/predeterminado")
     @ResponseStatus(HttpStatus.OK)
-    public Cliente setClientePredeterminado(@RequestBody Cliente cliente){
+    public Cliente setClientePredeterminado(@RequestBody Cliente cliente) {
        clienteService.setClientePredeterminado(cliente);
        return clienteService.getClientePorId(cliente.getId_Cliente());
     }
